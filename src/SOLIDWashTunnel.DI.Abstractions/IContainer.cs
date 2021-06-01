55e45fb3ceecea637d28b92bd68b898f90de3332
Ledjon Behluli <Ledjon@notiphy.io>
2021-06-01 23:33:09 +0200

    refactoring

--- src/SOLIDWashTunnel.DI.Abstractions/IContainer.cs
+++ src/SOLIDWashTunnel.DI.Abstractions/IContainer.cs
@@ -5,9 +5,9 @@ namespace SOLIDWashTunnel.DI.Abstractions
     public interface IContainer : IDisposable
     {
         void Register<TService, TImplementation>() where TImplementation : TService;
-        void Register<TService>(Func<TService> instanceCreator);
+        void Register<TService>(Func<TService> instanceFactory);
         void Register<TService>(TService instance);
-        void RegisterSingleton<TService>(Func<TService> instanceCreator);
+        void RegisterSingleton<TService>(Func<TService> instanceFactory);
         void Decorate<TService, TImplementation>() where TImplementation : TService;
         object GetService(Type type);
         TService GetService<TService>();
