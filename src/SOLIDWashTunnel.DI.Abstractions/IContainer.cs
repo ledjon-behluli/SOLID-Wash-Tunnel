832362b41c76ad57bf15f5d9c5c42d4c437c9766
Ledjon Behluli <Ledjon@notiphy.io>
2021-06-05 15:43:40 +0200

    wip

--- src/SOLIDWashTunnel.DI.Abstractions/IContainer.cs
+++ src/SOLIDWashTunnel.DI.Abstractions/IContainer.cs
@@ -5,9 +5,9 @@ namespace SOLIDWashTunnel.DI.Abstractions
     public interface IContainer
     {
         void Register<TService, TImplementation>() where TImplementation : TService;
-        void Register<TService>(Func<TService> instanceCreator);
+        void Register<TService>(Func<TService> instantiator);
         void Register<TService>(TService instance);
-        void RegisterSingleton<TService>(Func<TService> instanceCreator);
+        void RegisterSingleton<TService>(Func<TService> instantiator);
         void Decorate<TService, TImplementation>() where TImplementation : TService;
         object GetService(Type type);
         TService GetService<TService>();
--- src/SOLIDWashTunnel.DI/SimpleContainer.cs
+++ src/SOLIDWashTunnel.DI/SimpleContainer.cs
@@ -29,15 +29,15 @@ namespace SOLIDWashTunnel.DI
         public void Register<TService, TImplementation>() where TImplementation : TService
             => _registrations.Add(typeof(TService), () => GetService(typeof(TImplementation)));
 
-        public void Register<TService>(Func<TService> instanceCreator) 
-            => _registrations.Add(typeof(TService), () => instanceCreator());
+        public void Register<TService>(Func<TService> instantiator) 
+            => _registrations.Add(typeof(TService), () => instantiator());
 
         public void Register<TService>(TService instance) 
             => _registrations.Add(typeof(TService), () => instance);
 
-        public void RegisterSingleton<TService>(Func<TService> instanceCreator)
+        public void RegisterSingleton<TService>(Func<TService> instantiator)
         {
-            var lazy = new Lazy<TService>(instanceCreator);
+            var lazy = new Lazy<TService>(instantiator);
             Register(() => lazy.Value);
         }
 
@@ -60,8 +60,8 @@ namespace SOLIDWashTunnel.DI
 
         public object GetService(Type type)
         {
-            if (_registrations.TryGetValue(type, out Func<object> creator))
-                return creator();
+            if (_registrations.TryGetValue(type, out Func<object> instantiator))
+                return instantiator.Invoke();
             else if (!type.IsAbstract)
                 return CreateService(type);
             else
